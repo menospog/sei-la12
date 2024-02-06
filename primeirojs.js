982423127d86c4d5062e117d9a6af83109da31c8
Pedro <159148444+menospog@users.noreply.github.com>
2024-02-06 11:03:32 -0300

    Add files via upload

---
+++ primeirojs.js
@@ -0,0 +1,49 @@
+alert("Olá Mundo!");
+console.log('Olá Mundo!');
+document.write('Olá Mundo!')
+console.log(1+1)
+
+let name1= 'Luiz'
+name1 = "Arthur"
+console.log(name1)
+
+const name2='Luiz'
+console.log(name2)
+
+let firstname='Luiz'
+let lastname= 'Alberto'
+let fullname= firstname + '  ' + lastname
+console.log(fullname)
+
+let náme3= 2
+let name3= 1
+console.log(náme3)
+console.log(name3)
+
+
+
+
+
+
+
+
+const names = ['Arthur','Dúilio','Rafael','Ana','Luiz']
+console.log(names[0])
+console.log(names[3])
+console.log(names[2])
+console.log(names[1])
+console.log[names[4]]
+
+
+console.log(names.length)
+console.log(fullname.length)
+
+for (let i=0; i<names.length ; i++)
+{
+    console.log(names[1])
+}
+
+
+
+
+
